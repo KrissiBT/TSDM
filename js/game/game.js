9af5163a7119d4ae0320fcb9fdb84685d02e1b21
Hermann Björgvin Haraldsson <Hermann@hermann.is>
2015-03-03 03:57:45 +0000

    Finished assets preloader
    Now has a pretty simple callback system to use assets when ready,
    loading bar not yet done

--- js/game/game.js
+++ js/game/game.js
@@ -2,26 +2,42 @@
 
 define([
 	'jquery',
-	'underscore'
-], function($, _){
+	'underscore',
+	'modules/assets',
+	'modules/canvasHandler'
+], function($, _, moduleAssetsManager, moduleCanvasHandler){
 	var initialize = function(){
 
 		var canvas = document.getElementById('canvas');
 		var ctx = canvas.getContext('2d');
 
-		// Resize canvas and stuff
-        function reconfigureCanvas(){
-            canvas.height = canvas.offsetHeight;
-            canvas.width = canvas.offsetWidth;
-        }
-        window.onresize = function(){
-        	reconfigureCanvas();
-        };
-        reconfigureCanvas();
+		// Canvas handler, takes care of resizing and what knot
+		var canvasHandler = moduleCanvasHandler;
+		canvasHandler.configure(canvas);
+		canvasHandler.autoResize();
 
         // Test text plz ignore or remove
 		ctx.font = "40px Tahoma";
 		ctx.fillText("Test", 100, 100);
+
+		// Assets cache from preloader
+		var assetsManager = moduleAssetsManager;
+
+		assetsManager.preload([
+			'js/game/assets/professor_walk_cycle.png',
+			'js/game/assets/themomonstersprites018.png'
+		], function(cache){
+			console.log('Assets loaded');
+			assetsLoaded(cache);
+		});
+
+		function assetsLoaded(assets){
+			var charImg = assets['js/game/assets/professor_walk_cycle.png'];
+			ctx.drawImage(charImg, 100, 200);
+
+			var charImgA = assets['js/game/assets/themomonstersprites018.png'];
+			ctx.drawImage(charImgA, 700, 200);
+		}
 
 	};
 
--- js/game/modules/assets.js
+++ js/game/modules/assets.js
@@ -1,14 +1,7 @@
 // modules/assets
 
 // Handles downloading of assets and includes a callback when all assets are loaded
-
-/*
-	How i want to call the asset manager
-
-	Assets.preload([array of assets], function(){
-		// callback
-	})
-*/
+// Todo later, loading bar
 
 define([
 	'jquery',
@@ -20,13 +13,19 @@ define([
 	**************/
 
 	var downloadQueue = [];
+	var downloadCache = {};
 
-	function downloadFromPath(){
+	var successCount = 0;
+	var errorCount = 0;
 
-	}
-
-	function downloadAll(){
-
+	// Check if downloads are done
+	function isDone(){
+		if (successCount + errorCount === downloadQueue.length) {
+			return true;
+		}
+		else{
+			return false;
+		};
 	}
 
 	/*************
@@ -34,9 +33,42 @@ define([
 	*************/
 
 	var assetsApi = {};
-
+	
+	// Loop through downloadQueue and download to downloadCache
 	assetsApi.preload = function(pathArray, callback){
-		
+		// User defined paths
+		downloadQueue = pathArray;
+
+		// Loop and download
+		_.each(downloadQueue, function(path){
+
+			var img = new Image();
+			var self = this;
+
+			img.addEventListener('load', function(){
+				downloadCache[path] = img;
+				successCount += 1;
+
+				if(isDone()){
+					callback(downloadCache);
+				}
+			}, false);
+			img.addEventListener('error', function(){
+				errorCount += 1;
+
+				if(isDone()){
+					callback(downloadCache);
+				}
+			}, false);
+
+			img.src = path;
+
+		});
+
+		if (isDone()) {
+			// Callback with cache as parameter when download is finished
+			callback(downloadCache);
+		};
 	}
 
 	return assetsApi;
